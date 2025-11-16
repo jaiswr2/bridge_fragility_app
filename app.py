99b1a5c548db44942f691393c7d04fcb459bf678
jaiswr2 <jaiswr2@mcmaster.ca>
2025-11-16 01:03:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -205,9 +205,9 @@ surf = ax.plot_surface(
 )
 
 # Axis labels with normal spacing
-ax.set_xlabel("Foundation Scour Ratio (FSR)", fontsize=10, labelpad=10)
-ax.set_ylabel(y_choice, fontsize=10, labelpad=10)
-ax.set_zlabel("Probability of Exceedance", fontsize=10, labelpad=-2)
+ax.set_xlabel("Foundation Scour Ratio (FSR)", fontsize=8, labelpad=10)
+ax.set_ylabel(y_choice, fontsize=8, labelpad=10)
+ax.set_zlabel("Probability of Exceedance", fontsize=8, labelpad=-2)
 
 # Normal tick spacing
 ax.tick_params(axis='both', which='major', labelsize=8)
@@ -221,6 +221,7 @@ plt.tight_layout()
 col1, col2, col3 = st.columns([1, 2, 1])
 with col2:
     st.pyplot(fig)
+
 
 
 
